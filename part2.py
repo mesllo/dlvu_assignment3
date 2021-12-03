5bf71e2fab8cb6738a09f6cef1c02d69670d2afd
James Bonello <jamesbonello9@gmail.com>
2021-12-03 03:45:56 +0100

    added mnist cnn for q7 and q8, including some plot stuff for epoch loss, getting good accuracy but might need to implement on validation sets in case they expect us to do so

--- part2.py
+++ part2.py
@@ -16,6 +16,7 @@ print(device)
 # params
 batch_size = 16
 epochs = 1
+learning_rate = 0.001
 
 # dataset load
 train = torchvision.datasets.MNIST(root='./data', train=True, 
@@ -35,13 +36,88 @@ test = torchvision.datasets.MNIST(root='./data', train=False,
 testloader = torch.utils.data.DataLoader(test, batch_size=batch_size,
                                          shuffle=True, num_workers=0)
 
+# define CNN
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)        
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 3 * 3, 10)
+        
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.fc1(x)
+        return x
+    
+net = Net()
+net.to(device)
+
+# define loss function
+criterion = nn.CrossEntropyLoss()
+
+# define optimizer
+optimizer = optim.AdamW(net.parameters(), lr=0.001)
+
+# define loss lists to collect errors for plotting
+running_losses = []
+epoch_running_losses = []
+
 # train loop
 for epoch in range(epochs):  # loop over the dataset multiple times
 
     running_loss = 0.0
+    
     for i, data in enumerate(trainloader):        
         # use GPU
         inputs, labels = data[0].to(device), data[1].to(device)
         
-        print(i)
-        print(type(data))
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        # print statistics
+        running_loss += loss.item()
+        running_losses.append(running_loss/200)
+        if i % 200 == 0:    # print every 200 batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 200))
+            running_loss = 0.0
+    
+    epoch_running_losses.append(np.mean(running_losses))
+
+print('Finished Training')
+
+# plot running loss to show training behaviour changes
+plt.ylabel('Runinng loss')
+plt.xlabel('Epoch #')
+plt.xticks(range(len(epoch_running_losses)))
+plt.plot(epoch_running_losses)
+plt.title('Running loss of MNIST CNN for batch_size = ' + str(batch_size) + ', epochs = ' + str(epochs) + ', and lr = ' + str(learning_rate))
+plt.show()
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        # use GPU
+        images, labels = data[0].to(device), data[1].to(device)        
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
